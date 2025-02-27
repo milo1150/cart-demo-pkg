7f324a7860760466f41b02bfe9d1d952bdb81d65
milo33 <65894437+milo1150@users.noreply.github.com>
2025-02-27 18:35:21 +0700

    init

---
+++ pkg/error.go
@@ -0,0 +1,19 @@
+package pkg
+
+import (
+	"github.com/go-playground/validator"
+	"github.com/samber/lo"
+)
+
+func TranslateErrors(errors validator.ValidationErrors) map[string]string {
+	var errorMessage = make(map[string]string)
+	for _, err := range errors {
+		key := lo.SnakeCase(err.Field())
+		errorMessage[key] = err.Tag()
+	}
+	return errorMessage
+}
+
+func GetSimpleErrorMessage(msg string) map[string]string {
+	return map[string]string{"error": msg}
+}
