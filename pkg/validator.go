94d0d575726135ff6429e6db05b500ce2c47145d
milo33 <65894437+milo1150@users.noreply.github.com>
2025-02-27 18:43:12 +0700

    add: ValidateJsonPayload

---
+++ pkg/validator.go
@@ -0,0 +1,11 @@
+package pkg
+
+import "github.com/go-playground/validator"
+
+func ValidateJsonPayload(validate *validator.Validate, payload interface{}) map[string]string {
+	if err := validate.Struct(payload); err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		return TranslateErrors(validationErrors)
+	}
+	return nil
+}
